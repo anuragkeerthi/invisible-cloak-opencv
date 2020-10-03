5f43e15eaf7c8f0d6e4040a9d9e8b47200414d8b
Anurag <anurag@pop-os.localdomain>
2020-10-03 23:02:02 +0530

    Added Invisible Cloak File

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -1,15 +1,18 @@
 import cv2
+import numpy as np 
 
 capture = cv2.VideoCapture(0)
+background_image = cv2.imread('./image.png')
 
 while capture.isOpened():
     ret, frame = capture.read()
+    
     if ret:
-        cv2.imshow("image", frame)
-        if cv2.waitKey(5) == ord('q'):
-            cv2.imwrite('image.jpg', frame)
-            break
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv", hsv)
 
+        if cv2.waitKey(5) == ord('q'):
+            break
 
 capture.release()
 cv2.destroyAllWindows()
