9ff6d6bccfa6d22f56e21c6c395db8f6c99c6840
Anurag <anurag@pop-os.localdomain>
2020-10-03 23:13:35 +0530

    Converting RGB to HSV

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -9,7 +9,9 @@ while capture.isOpened():
     
     if ret:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        cv2.imshow("hsv", hsv)
+        #cv2.imshow("hsv", hsv)
+        rgb_colour = np.unit8([[[0,0,255]]])
+        hsv_colour = cv2.cvtColor(rgb_colour, cv2.COLOR_BGR2HSV)
 
         if cv2.waitKey(5) == ord('q'):
             break
